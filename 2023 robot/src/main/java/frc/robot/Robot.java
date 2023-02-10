d5f7dd92ccce4ad9211d4220bb37445972cec1a8
BetaBlues <49037537+BetaBlues@users.noreply.github.com>
2023-02-09 17:15:20 -0800

    smartdash2023

--- 2023 robot/src/main/java/frc/robot/Robot.java
+++ 2023 robot/src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -20,12 +21,9 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   public static RobotContainer m_robotContainer;
+  public PowerDistribution m_pdh;
   
   
-  protected void execute() {
-    //SmartDashboard.putNumber("Gyro Direction", m_robotContainer.gyro.getAngle());
-    //SmartDashboard.putNumber("Front Right Voltage ", m_robotContainer.m_chassis.rightFrontMotor.getBusVoltage());
-  }
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -35,6 +33,8 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
+    m_pdh = new PowerDistribution();
     
   }
 
@@ -52,11 +52,22 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+
+    SmartDashboard.putNumber("Voltage", m_pdh.getVoltage());
+    SmartDashboard.putNumber("Gyro Direction", m_robotContainer.gyro.getAngle());
+    SmartDashboard.putNumber("Front Right Voltage", m_robotContainer.m_chassis.rightFrontMotor.getBusVoltage());
+    SmartDashboard.putNumber("Front Left Voltage", m_robotContainer.m_chassis.leftFrontMotor.getBusVoltage());
+    SmartDashboard.putNumber("Back Right Voltage", m_robotContainer.m_chassis.rightRearMotor.getBusVoltage());
+    SmartDashboard.putNumber("Back Left Voltage", m_robotContainer.m_chassis.leftRearMotor.getBusVoltage());
+     
+
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+        
+  }
 
   @Override
   public void disabledPeriodic() {}
@@ -95,12 +106,14 @@ public class Robot extends TimedRobot {
   public void testInit() {
     // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
+   
     //execute();
   }
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 
   /** This function is called once when the robot is first started up. */
   @Override
